b99ba5f5bf38d7432277032dc9c13774281553c7
tquangdo <trangia61@gmail.com>
2022-03-07 13:03:10 +0900

    first commit

---
+++ terraform-start/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  s3_origin_id = "access-identity-serverless-series-spa"
+  region       = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "base_url" {
+  value = {
+    api_staging = aws_api_gateway_deployment.staging.invoke_url
+    api_production = aws_api_gateway_deployment.production.invoke_url
+    web = aws_cloudfront_distribution.s3_distribution.domain_name
+  }
+}
